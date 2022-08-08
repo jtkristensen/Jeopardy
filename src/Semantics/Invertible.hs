69bce35112aaa6babfbd57964c31a8f525f3bade
Joachim Tilsted Kristensen <joachim.tilsted@gmail.com>
2022-08-08 20:58:21 +0200

    initial module : Semantics

---
+++ src/Semantics/Invertible.hs
@@ -0,0 +1,16 @@
+{-|
+
+Module      : Semantics.Invertible
+Description : Program level invertible semantics for Jeopardy.
+Author      : Joachim Tilsted Kristensen.
+Licence     : GNU GENERAL PUBLIC LICENSE
+Maintainer  : joachkr@ifi.uio.no
+Stability   : experimental
+Portability : POSIX
+
+A conservative extension of the reversible semantics for Jeopardy, that only
+guarantees invertibility globally.
+
+-}
+
+module Semantics.Invertible where
---
+++ src/Semantics/Reversible.hs
@@ -0,0 +1,16 @@
+{-|
+
+Module      : Semantics.Reversible
+Description : A per function invertible semantics for Jeopardy.
+Author      : Joachim Tilsted Kristensen.
+Licence     : GNU GENERAL PUBLIC LICENSE
+Maintainer  : joachkr@ifi.uio.no
+Stability   : experimental
+Portability : POSIX
+
+An interpreter for Jeopardy that only accepts linearly typable programs, and
+on the other hand promises that functions are locally invertible.
+
+-}
+
+module Semantics.Reversible where
