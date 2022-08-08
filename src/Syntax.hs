ad76a849094f2af10b58a400639dc10c2d50870e
Joachim Tilsted Kristensen <joachim.tilsted@gmail.com>
2022-08-08 23:34:27 +0200

    more elaborate syntax

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -14,30 +14,13 @@ article, `Jeopardy : an invertible programming language`, submitted to the
 
 -}
 
-module Syntax
-  ( desugar
-  , Program(..)
-  , Pattern(..)
-  , Term(..)
-  , Inversion(..)
-  ) where
+module Syntax where
 
-import           Core.Syntax (Name, X, C, F)
-import qualified Core.Syntax as Core
-
-import Control.Monad.State  (State(..) , runState )
-import Control.Monad.Writer (Writer(..), runWriter)
+import Core.Syntax (Name, X, C, F)
 
 -- It has been decided that types have upper case capitalization, while type
 -- varibles have lower case. We resolve polymorphism like templates.
 data T = ConcreteType Name | TypeVariable Name
-
--- We generate built-in tuples for each type, for each size.
--- Likewise for built-in lists.
-data Context =
-  Context { max_tuple_size :: Integer
-          , datatype_names :: [T]
-          }
 
 data Program
   = Function F (Pattern, T) (Term, T)
@@ -65,26 +48,3 @@ data Term
 data Inversion
   = Conventional F
   | Invert Inversion
-
-desugar :: Program -> Core.Program ()
-desugar = undefined
-
--- desugarPattern :: Pattern -> Core.Pattern ()
--- desugarPattern (Variable    x   )   = Core.Variable x ()
--- desugarPattern (Constructor c ps)   = Core.Constructor c (map desugarPattern ps) ()
--- desugarPattern (NilPattern      )   = Core.Constructor "$built-in-nil" [] ()
--- desugarPattern (ConsPattern x xs)   = Core.Constructor "$built-in-cons" [x', xs'] ()
---   where x'  = desugarPattern x
---         xs' = desugarPattern xs
--- desugarPattern (TuplePattern p1 p2) = Core.Constructor "$built-in-tuple" [q1, q2] ()
---   where q1 = desugar p1
---         q2 = desugar p2
-
--- desugarTerm :: Term -> Core.Term ()
--- desugarTerm (VariableTerm    x   ) = Core.Pattern (desugarPattern (Variable x))
--- desugarTerm (ConstructorTerm c ts) = Core.Pattern (
-
--- desugarInversion :: Inversion -> Core.Inversion             ()
--- desugarInversion (Conventional f) = Core.Conventional f     ()
--- desugarInversion (Invert       g) = Core.Invert (desugar g) ()
-
