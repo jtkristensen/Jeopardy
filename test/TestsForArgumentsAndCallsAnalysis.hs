13819f9bb5a6efc5b189c2907c6b4fc0085d139a
FurryRogue <joachim.tilsted@gmail.com>
2022-08-24 14:57:03 +0200

    comply with hlint

--- test/TestsForArgumentsAndCallsAnalysis.hs
+++ test/TestsForArgumentsAndCallsAnalysis.hs
@@ -12,38 +12,33 @@ import ExamplePrograms
 
 import Control.Monad (void)
 
--- import Data.Either
--- import Data.List (sort)
-
 import Analysis.ArgumentsAndCalls
 
 problematicArgumentOrCallAnalysisTests :: TestTree
 problematicArgumentOrCallAnalysisTests =
-  testGroup ""
+  testGroup "Unit tests about arguments and calls analysis."
     [ numberOfArgumentsTests
     ]
-
 
 numberOfArgumentsTests :: TestTree
 numberOfArgumentsTests =
   testGroup "Tests about lookups in the program text."
   [ testCase "the [zero] constructor takes no arguments" $
     fromExample firstProgram $
-    \first ->  numberOfArguments "zero" first @?= (return 0)
+    \first ->  numberOfArguments "zero" first @?= return 0
   , testCase "the [suc _] constructor takes one arguments" $
     fromExample firstProgram $
-    \first ->  numberOfArguments "suc" first @?= (return 1)
+    \first ->  numberOfArguments "suc" first @?= return 1
   , testCase "the [pair _ _] constructor takes two arguments" $
     fromExample firstProgram $
-    \first ->  numberOfArguments "pair" first @?= (return 2)
+    \first ->  numberOfArguments "pair" first @?= return 2
   , testCase "Nothing to be said about constructors that are not declared." $
     fromExample firstProgram $
     \first ->  numberOfArguments "wut" first @?= Nothing
   ]
 
-
 fromExample :: Source -> (Program () -> Assertion) -> Assertion
 fromExample src f =
   case parseString program_ src of
-    Right p -> f $ (void p)
+    Right p -> f $ void p
     _       -> assertBool ("unable to parse " ++ src) False
