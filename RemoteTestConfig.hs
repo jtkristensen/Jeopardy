a309a8d4f5333aec9fc113cf0217663b4e22bb9f
FurryRogue <joachim.tilsted@gmail.com>
2022-08-26 19:47:07 +0200

    testing local/remote test configs

--- RemoteTestConfig.hs
+++ RemoteTestConfig.hs
@@ -3,8 +3,8 @@ module TestConfig where
 -- This is an argument for QuickCheck, if it is larger, then the testcases
 -- are larger, but they take significantly more time to run.
 sizeOfGeneratedPatterns :: Int
-sizeOfGeneratedPatterns = 5
+sizeOfGeneratedPatterns = 8
 
 -- This decides how many testcases QuickCheck should run.
 numberOfTests :: Int
-numberOfTests = 100
+numberOfTests = 10000
